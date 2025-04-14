ac9d0cc4f693a46c28ec9ca09e7eaa042ad5064f
Adriano Maringolo <adrianomaringolo@gmail.com>
2025-04-14 00:06:20 -0300

    change about section color

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,6 +1,6 @@
 export const About = () => {
   return (
-    <section className="py-16 px-6 md:px-12 bg-gray-800 text-white relative overflow-hidden">
+    <section className="py-16 px-6 md:px-12 bg-[#2c2c2c] text-white relative overflow-hidden">
       <div className="absolute w-60 h-60 bg-white rounded-full blur-3xl opacity-50 -right-50 -top-30" />
       <div className="absolute w-60 h-60 bg-white rounded-full blur-3xl opacity-50 -left-50 -bottom-30" />
       <div className="max-w-6xl mx-auto">
