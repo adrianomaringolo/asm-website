4ea74e0417742ce5c099af3d1d154082cffee13d
Adriano Maringolo <adrianomaringolo@gmail.com>
2025-04-15 14:14:52 -0300

    fix button border style

--- src/components/CTAEbookButton.tsx
+++ src/components/CTAEbookButton.tsx
@@ -5,11 +5,10 @@ import { ReactNode } from "react";
 export function CTAEbookButton({
   text = (
     <>
-      <span className="font-light">Baixe GRATUITAMENTE o nosso ebook</span>
+      <span className="font-light">Baixe GRATUITAMENTE o nosso e-book</span>
       <br />
       <strong className="font-bold text-xl">
-        E-book 5 Prompts de IA para Criar Conteúdo Rápido, Criativo e
-        Estratégico
+        5 Prompts de IA para Criar Conteúdo Rápido, Criativo e Estratégico
       </strong>
     </>
   ),
@@ -21,12 +20,12 @@ export function CTAEbookButton({
       href="https://docs.google.com/forms/d/e/1FAIpQLSf99m1mq3QJiDoZ4uSAoNC1G8ui92BXoPkd3wTQ3k7TrAA3lw/viewform?usp=dialog"
       className={clsx(
         "block bg-gradient-to-r from-[#DDCC70] to-[#776E3C]",
-        "font-medium px-1 py-1 rounded hover:opacity-90 transition-all",
+        "font-medium px-1 py-1 rounded-[15px] hover:opacity-90 transition-all",
         "hover:shadow-2xl hover:shadow-[#776E3C]",
         "cursor-pointer w-full"
       )}
     >
-      <span className="bg-white flex items-center gap-4 w-full rounded px-6 py-5 hover:bg-white/80">
+      <span className="bg-white flex items-center gap-4 w-full rounded-[12px] px-6 py-5 hover:bg-white/80 transition-colors">
         <Book size={50} className="min-w-[50px]" />
         <span className="text-left leading-5">{text}</span>
       </span>
